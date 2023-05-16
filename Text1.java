25f52258d4d38bd704ca918d3a970979e14a80ed
Anuhya3 <133735334+Anuhya3@users.noreply.github.com>
2023-05-16 10:05:06 +0530

    Add files via upload

---
+++ Text1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+public class Text1{
+public static void main(String []args){
+System.out.print(args[0] + " ");
+System.out.print("Technologies"+ " ");
+System.out.print(args[1]);
+}
+}
+
